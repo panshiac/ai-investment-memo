25cfcbecdb87006064ce57570f5dcf36e3338739
panshiac <pshiacolas@gmail.com>
2026-07-04 16:04:45 +0300

    ljawf

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ st.markdown(
 _, col, _ = st.columns([2, 5, 2])
 
 with col:
-    st.image("logo.png", width=700)
+    st.image("logo.png", width=900)
 
 st.markdown("<br>", unsafe_allow_html=True)
 
